7cc9595b6f3421dde0f39b0e350cc475367c134f
LKerry <annekerrieleveille@yahoo.fr>
2017-08-12 12:14:49 -0400

    first
    push

---
+++ app/src/main/java/com/codepath/apps/mysimpletweets/ProfileActivity.java
@@ -0,0 +1,8 @@
+package com.codepath.apps.mysimpletweets;
+
+/**
+ * Created by labadmin on 8/12/17.
+ */
+
+public class ProfileActivity {
+}
---
+++ app/src/main/java/com/codepath/apps/mysimpletweets/fragment/MentionsTimelineFragment.java
@@ -0,0 +1,50 @@
+package com.codepath.apps.mysimpletweets.fragment;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import com.codepath.apps.mysimpletweets.TwitterApplication;
+import com.codepath.apps.mysimpletweets.TwitterClient;
+import com.codepath.apps.mysimpletweets.models.Tweet;
+import com.loopj.android.http.JsonHttpResponseHandler;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import cz.msebera.android.httpclient.Header;
+
+/**
+ * Created by labadmin on 8/12/17.
+ */
+
+public class MentionsTimelineFragment extends TweetsListFragment {
+    private TwitterClient client;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        client = TwitterApplication.getRestClient();
+        populateTimeline();
+
+    }
+
+    private void populateTimeline() {
+        client.getMentionsTimeline(new JsonHttpResponseHandler() {
+            //SUCCESS
+
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONArray json) {
+                // Log.d("DEBUG", json.toString());
+                addAll(Tweet.fromJSONArray(json));
+            }
+
+
+            //FAILURE
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+                Log.d("DEBUG", errorResponse.toString());
+            }
+        });
+    }
+}
